207548d1d3f0dbc0a36466aafd90b18c2f597c1d
Kim Carter <Kim.Carter@binarymist.net>
2018-07-15 15:59:09 +1200

    Tidied up list & screen

--- src/cmds/list.js
+++ src/cmds/list.js
@@ -1,8 +1,5 @@
 const config = require('config/config');
-const log = require('purpleteam-logger').logger();
-debugger;
 const { list: listApi, common: commonApi } = require('src/api');
-debugger;
 
 exports.flags = `list [order=${config.get('displayOrderOfListMemberScheduledSends')}]`;
 exports.desc = 'List members in order based on latest or oldest mailgunMateScheduledSends datetimes.';
@@ -12,21 +9,17 @@ exports.params = [{
   choices: ['des', 'asc']
 }];
 exports.setup = (sywac) => {
-  debugger;
-  sywac  
-  .option(
-    '-l, --email-list <email-list>',
-    {
-      type: 'string', desc: 'The mailgun email list you would like to use.', defaultValue: config.get('emailList')
-    }
-  );
-};  
-exports.run = async (parsedArgv, context) => {      
-  debugger;
-  commonApi.setListMemberDispalyOrder(parsedArgv.order);      
-  commonApi.setMailList(parsedArgv.l);   
-
+  sywac
+    .option(
+      '-l, --email-list <email-list>',
+      {
+        type: 'string', desc: 'The mailgun email list you would like to use.', defaultValue: config.get('emailList')
+      }
+    );
+};
+exports.run = async (parsedArgv, context) => {
+  commonApi.setListMemberDispalyOrder(parsedArgv.order);
+  commonApi.setMailList(parsedArgv.l);
   await commonApi.authenticateToMailgun();
-  debugger;
   await listApi.displaySubscribedListMembers();
 };
--- src/cmds/screen.js
+++ src/cmds/screen.js
@@ -5,20 +5,17 @@ exports.flags = '*';
 exports.desc = 'Default command for about and help.';
 exports.setup = {};
 exports.run = (parsedArgv, context) => {
-  const argv = parsedArgv;
-  argv.handled = true;
-  debugger;
   if (parsedArgv.about) {
     const {
       name: projectName, version, description, homepage, author: { name, email }
     } = pkg;
 
-    log.notice(`${projectName} ${version}`);
-    log.notice(description);
-    log.notice(`Homepage: ${homepage}`);
-    log.notice(`Created by ${name}<${email}>\n`);
+    log.notice(`${`${projectName}`.padEnd(15)} ${version}`);
+    log.notice(`${'Description:'.padEnd(15)} ${description}`);
+    log.notice(`${'Homepage:'.padEnd(15)} ${homepage}`);
+    log.notice(`${'Created by:'.padEnd(15)} ${name}<${email}>\n`);
   } else {
     return context.cliMessage(`Unknown argument: ${context.args}`);
   }
-  return argv;
+  return parsedArgv;
 };
