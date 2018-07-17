f1f27b18141719e4982072d4ad37b9e81172174d
Kim Carter <Kim.Carter@binarymist.net>
2018-07-17 18:13:51 +1200

    Linted list

--- src/api/list.js
+++ src/api/list.js
@@ -4,16 +4,11 @@ const commonApi = require('src/api/common');
 
 const displaySubscribedListMembers = async () => {
   await commonApi.establishSubscribedListMembersAndSort((orderedDisplayableSubscribedListMembers) => {
-    log.notice(`\n${'Ordered Subscribed Members'.padEnd(config.get('valueToSiblingFieldPadWidth'))}DateTime Scheduled` + '\n' + 
-    orderedDisplayableSubscribedListMembers.reduce(
-      (accumulated, member) => 
-        `${accumulated}\n${`${member.address}`.padEnd(config.get('valueToSiblingFieldPadWidth'))}${member.latestScheduledSend}`
-        , ''
-    ));
+    // eslint-disable-next-line prefer-template
+    log.notice(`\n${'Ordered Subscribed Members'.padEnd(config.get('valueToSiblingFieldPadWidth'))}DateTime Scheduled\n` +
+    orderedDisplayableSubscribedListMembers.reduce((accumulated, member) => `${accumulated}\n${`${member.address}`.padEnd(config.get('valueToSiblingFieldPadWidth'))}${member.latestScheduledSend}`, ''));
   });
 };
 
 
-module.exports = {  
-  displaySubscribedListMembers
-};
+module.exports = { displaySubscribedListMembers };
